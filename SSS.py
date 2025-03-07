e4f30cf0f80ab7928b302aec28d3ff2d5b865987
Nama-gem <raphael.kaubruegger@gmail.com>
2025-03-07 16:33:30 -0700

    Bug fix

--- SSS.py
+++ SSS.py
@@ -40,9 +40,13 @@ for i in np.arange(0, len(S_)):
     m = np.kron(np.arange(S, - S - 1, - 1), np.ones(int(2 * S + 1))) + np.kron(np.ones(int(2 * S + 1)), np.arange(S, - S - 1, - 1))
     ind_rho = np.concatenate([np.array(list(combinations(np.argwhere(m == m1).ravel(), 2))) for m1 in np.arange(2 * S - 1, - 2 * S, - 1)])
 
+    # res = minimize(lambda x: F_prod_der(x[0], oat(x[1], S, Bx), oat(x[1], S, Bx), basis, ind_rho), np.array([phi[i - 1], mu[i - 1]]), jac = True, method = method,
+    #               bounds = Bounds([0, 0], [np.pi / 8, mu[np.max([i - 2, 0])]]))
 
-    res = minimize(lambda x: F_prod_der(x[0], oat(x[1], S, Bx), oat(x[1], S, Bx), basis, ind_rho), np.array([phi[i - 1], mu[i - 1]]), jac = True, method = method,
-                  bounds = Bounds([0, 0], [np.pi / 8, mu[np.max([i - 2, 0])]]))
+    res = minimize(lambda x: F_prod_der(x[0], oat(x[1], S, Bx), oat(x[1], S, Bx), basis, ind_rho), np.array([phi[i - 1], 0.1915599]), jac = True, method = method,
+                  bounds = Bounds([0, 0], [np.pi / 8, 0.25]))
+    # mu[np.max([i - 2, 0])]
+    # 0.1915599
 
     # res = minimize(lambda x: F_prod_fast_der(x[0], oat(x[1], S, Bx), oat(x[1], S, Bx), nPhi, Bx), np.array([phi[i - 1], mu[i - 1]]), 
     #                jac = True, method = method, bounds = Bounds([0, 0], [np.pi / 8, mu[np.max([i - 2, 0])]]))
